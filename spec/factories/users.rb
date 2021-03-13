f12736d615314a8e2154f3ad9e326eac4cef8d32
K-OSG <gongonta6886@gmail.com>
2021-03-13 11:35:23 +0900

    ユーザー管理機能実装修正２回目

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,8 +3,7 @@ FactoryBot.define do
     gimei = Gimei.name
     nickname {Faker::Name.last_name}
     email {Faker::Internet.free_email}
-    password = '1a' + Faker::Internet.password(min_length: 6)
-    password {password}
+    password {'1a' + Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
     fristname {gimei.first.kanji}
     lastname {gimei.last.kanji}
